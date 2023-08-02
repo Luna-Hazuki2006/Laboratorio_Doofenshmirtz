ea0caa786e35a6d816b7488f34e188c08ac8e45b
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-08-02 12:36:12 -0400

    con la mayoria de metodos pero no todos

--- app.py
+++ app.py
@@ -6,22 +6,64 @@ app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = ')xf2PLw8._gUkJ?~'
 
 @app.route('/', methods=['GET'])
-def listar_servicios():
+def listar_examenes():
     pass
 
 @app.route('/crear_examen', methods=['GET', 'POST'])
 def crear_examen():
     pass
 
-@app.route('/<id>/actualizar', methods=['GET', 'POST'])
+@app.route('/<id>/consultar_examen', methods=['GET'])
+def consultar_examen(id):
+    pass
+
+@app.route('/<id>/actualizar_examen', methods=['GET', 'POST'])
 def actualizar_examen(id):
     pass
 
-@app.route('/<id>/eliminar', methods=['GET', 'POST'])
+@app.route('/<id>/eliminar_examen', methods=['GET', 'POST'])
 def eliminar_examen(id):
     pass
 
+@app.route('/listar_indicaciones', methods=['GET'])
+def listar_indicaciones():
+    pass
 
+@app.route('/crear_indicacion', methods=['GET', 'POST'])
+def crear_indicacion():
+    pass
+
+@app.route('/<id>/consultar_indicacion', methods=['GET'])
+def consultar_indicacion(id):
+    pass
+
+@app.route('/<id>/actualizar_indicacion', methods=['GET', 'POST'])
+def actualizar_indicacion(id):
+    pass
+
+@app.route('/<id>/eliminar_indicacion', methods=['GET', 'POST'])
+def eliminar_indicacion(id):
+    pass
+
+@app.route('/listar_categorias', methods=['GET'])
+def listar_categorias():
+    pass
+
+@app.route('/crear_categorias', methods=['GET', 'POST'])
+def crear_categorias():
+    pass
+
+@app.route('/<id>/consultar_categoria', methods=['GET'])
+def consultar_categorias(id):
+    pass
+
+@app.route('/<id>/actualizar_categoria', methods=['GET', 'POST'])
+def actualizar_categoria(id):
+    pass
+
+@app.route('/<id>/eliminar_categoria', methods=['GET', 'POST'])
+def eliminar_categoria(id):
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
